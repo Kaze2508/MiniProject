e286c3e0fb457d53e80a130734f17feb757de932
Kaze Phake <phongsgvn@gmail.com>
2023-06-15 18:24:02 +0700

    Updated MQTT

--- app/app.py
+++ app/app.py
@@ -2,20 +2,22 @@ from flask import Flask, render_template
 from flask_mqtt import Mqtt
 
 app = Flask(__name__)
-app.config['MQTT_BROKER_URL'] = 'mybroker.com'
+app.config['MQTT_BROKER_URL'] = 'mqtt.flespi.io'
 app.config['MQTT_BROKER_PORT'] = 1883
-app.config['MQTT_USERNAME'] = 'user'
-app.config['MQTT_PASSWORD'] = 'secret'
+app.config['MQTT_USERNAME'] = '8UfmqqhmM4rC04D0mU4gfocqPDluQ93OR9Y5n5fZXxrOrUEBDwIIKKXpb8HR6AvL'
+app.config['MQTT_PASSWORD'] = ''
 app.config['MQTT_REFRESH_TIME'] = 1.0
-mqtt = Mqtt(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+topic = 'topic/subtopic'
+mqtt = Mqtt(app)
 
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
-    mqtt.subscribe('home/mytopic')
+    if rc == 0:
+        print('Connected successfully')
+        mqtt_client.subscribe(topic) # subscribe topic
+    else:
+        print('Bad connection. Code:', rc)
 
 @mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
@@ -23,6 +25,13 @@ def handle_mqtt_message(client, userdata, message):
         topic=message.topic,
         payload=message.payload.decode()
     )
+    print('Received message on topic: {topic} with payload: {payload}'.format(**data))
+
+@app.route('/publish', methods=['POST'])
+def publish():
+    data = request.get_json()
+    mqtt_client.publish(topic, data['message'])
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
