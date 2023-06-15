cee961aeb6fbb5f03de5d417047c3e2925e3e205
Kaze Phake <phongsgvn@gmail.com>
2023-06-16 05:53:44 +0700

    Updated

--- app/app.py
+++ app/app.py
@@ -3,6 +3,8 @@ from flask_mqtt import Mqtt
 from flask import jsonify
 import sqlite3
 import json
+from bokeh.embed import components
+from bokeh.plotting import figure
 
 app = Flask(__name__)
 app.config['MQTT_BROKER_URL'] = 'mqtt.flespi.io'
@@ -36,14 +38,36 @@ def handle_message(client, userdata, msg):
     conn.commit()
     conn.close()
 
+# @app.route('/')
+# def index():
+#     conn = sqlite3.connect('database.db')
+#     c = conn.cursor()
+#     c.execute("SELECT * FROM data")
+#     data = c.fetchall()
+#     conn.close()
+#     return render_template('index.html', data=data)
+
 @app.route('/')
-def index():
-    conn = sqlite3.connect('database.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM data")
-    data = c.fetchall()
-    conn.close()
-    return render_template('index.html', data=data)
+def homepage():
+ 
+    # Define Plot Data
+    labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+    ]
+ 
+    data = [0, 10, 15, 8, 22, 18, 25]
+ 
+    # Return the components to the HTML template
+    return render_template(
+        template_name_or_list='chartjs-example.html',
+        data=data,
+        labels=labels,
+    )
 
 @app.route('/my_route')
 def my_route():
