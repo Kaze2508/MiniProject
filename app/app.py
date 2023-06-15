cb922022a5ec1efcc352e8162c5de08836aefbdc
Kaze Phake <phongsgvn@gmail.com>
2023-06-15 18:00:58 +0700

    Updated MQTT

--- app/app.py
+++ app/app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template
+from flask_mqtt import Mqtt
+
 app = Flask(__name__)
+app.config['MQTT_BROKER_URL'] = 'mybroker.com'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_USERNAME'] = 'user'
+app.config['MQTT_PASSWORD'] = 'secret'
+app.config['MQTT_REFRESH_TIME'] = 1.0
+mqtt = Mqtt(app)
 
 @app.route('/')
 def index():
@@ -7,3 +15,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
